ffe95ac4b34accef3fce9f0d41da76ba1ff27415
tommckee96 <57681416+tommckee96@users.noreply.github.com>
2023-05-23 12:55:09 +0100

    Update streamlit_app.py
    changed header and body text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Streamlit App')
-streamlit.header('Header text')
-streamlit.text('Body text')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.text('More body text')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
